90a2c30dcc04d74fdae8ad5acd9a5a2b3bcf3f03
Jonatan Sombié <jonatan.sombie@gmail.com>
2023-04-25 16:47:38 +0200

    use target env name as output names

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 terraform {
-  #  backend "s3" {}
+  backend "s3" {}
 
   required_providers {
     aws = {
@@ -152,7 +152,7 @@ output "REDIS_USER" {
 
 data "aws_region" "current" {}
 
-output "REDIS_PASSWORD_REF" {
+output "REDIS_PASSWORD" {
   value = {
     type   = "ssm"
     key    = local.redis_password_parameter_key
